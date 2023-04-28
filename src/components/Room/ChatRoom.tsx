4d79ec82ac3a774dbc4a222ef24920b8c995e550
donykevin214 <donykevin214@gmail.com>
2023-04-29 00:59:34 +0100

    chore: cleanup

--- src/components/Room/ChatRoom.tsx
+++ src/components/Room/ChatRoom.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import { Input, Button } from '@/components/UI'
+import React from "react";
+import { Input, Button } from "@/components/UI";
 export const ChatRoom: React.FC = () => {
-    return(
-        <div className='relative col-span-2 border border-sky-600 mx-2 bg-orange-100'>
-            <p className='w-full px-4 py-4 bg-orange-300 border-b text-bold '>
-                Chat Room
-            </p>
-            <div className='absolute w-full bottom-4 flex justify-between h-100 flex px-2 gap-2'>
-                <Input type = 'text' className='w-8/12 border-black h-[40px]'/>
-                <Button text='Send' bg_color='bg-purple-100' className= 'w-4/12 h-[40px]' />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="relative col-span-2 border border-sky-600 mx-2 bg-orange-100">
+      <p className="w-full px-4 py-4 bg-orange-300 border-b text-bold ">
+        Chat Room
+      </p>
+      <div className="absolute w-full bottom-4 justify-between h-100 flex px-2 gap-2">
+        <Input type="text" className="w-8/12 border-black h-[40px]" />
+        <Button
+          text="Send"
+          bg_color="bg-purple-100"
+          className="w-4/12 h-[40px]"
+        />
+      </div>
+    </div>
+  );
+};
