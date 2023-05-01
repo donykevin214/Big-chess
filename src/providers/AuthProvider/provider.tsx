b52c5bd11cf0f2c1bb06fd966a38054035f8a535
donykevin214 <donykevin214@gmail.com>
2023-05-01 17:48:43 +0100

    chore: add authentication provider

--- src/providers/AuthProvider/provider.tsx
+++ src/providers/AuthProvider/provider.tsx
@@ -64,7 +64,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({
     data: user,
     refetch,
     isLoading,
-  } = useTrpcQuery<{}, PublicSession>("auth.me", {});
+  } = useTrpcQuery<any, PublicSession>("auth.me", {});
 
   const { mutate } = useMutation({
     mutationFn() {
