00be0bd1d0d1ee5764de6eb61e6b254072a61edf
donykevin214 <donykevin214@gmail.com>
2023-05-01 17:48:03 +0100

    chore: update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,17 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: [
-    "airbnb",
-    "airbnb-typescript",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module", project: './tsconfig.json' },
-  plugins: ["react-refresh"],
+  extends: ['react-app', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
   rules: {
-    "react-refresh/only-export-components": "warn",
+    'react-refresh/only-export-components': 'warn',
+    'no-debugger': 'warn',
+    'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+    '@typescript-eslint/no-unused-vars': 'error',
+    'react-hooks/exhaustive-deps': 'off',
   },
 };
